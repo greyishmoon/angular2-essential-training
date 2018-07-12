fa229a49bdb8e5f313e202c845f11804f26da2f9
greyishmoon <chris@blythe.plus.com>
2018-07-12 18:28:51 +0100

    Validation + error handling

--- app/media-item-form.component.ts
+++ app/media-item-form.component.ts
@@ -2,40 +2,45 @@ import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'mw-media-item-form',
-  templateUrl: 'app/media-item-form.component.html',
-  styleUrls: ['app/media-item-form.component.css']
+    selector: 'mw-media-item-form',
+    templateUrl: 'app/media-item-form.component.html',
+    styleUrls: ['app/media-item-form.component.css']
 })
 export class MediaItemFormComponent {
-  form;
+    form;
 
-  ngOnInit() {
-    this.form = new FormGroup({
-      medium: new FormControl('Movies'),
-      name: new FormControl('', Validators.compose([
-        Validators.required,
-        Validators.pattern('[\\w\\-\\s\\/]+')
-      ])),
-      category: new FormControl(''),
-      year: new FormControl('', this.yearValidator),
-    });
-  }
-
-  yearValidator(control) {
-    if(control.value.trim().length === 0) {
-      return null;
+    ngOnInit() {
+        this.form = new FormGroup({
+            medium: new FormControl('Movies'),
+            name: new FormControl('', Validators.compose([
+                Validators.required,
+                Validators.pattern('[\\w\\-\\s\\/]+')
+            ])),
+            category: new FormControl(''),
+            year: new FormControl('', this.yearValidator),
+        });
     }
-    let year = parseInt(control.value);
-    let minYear = 1900;
-    let maxYear = 2100;
-    if(year >= minYear && year <= maxYear) {
-      return null;
-    } else {
-      return { 'year': true };
-    }
-  }
 
-  onSubmit(mediaItem) {
-    console.log(mediaItem);
-  }
+    yearValidator(control) {
+        if (control.value.trim().length === 0) {
+            return null;
+        }
+        let year = parseInt(control.value);
+        let minYear = 1800;
+        let maxYear = 2500;
+        if (year >= minYear && year <= maxYear) {
+            return null;
+        } else {
+            return {
+                'year': {
+                    'min': minYear,
+                    'max': maxYear
+                }
+            };
+        }
+    }
+
+    onSubmit(mediaItem) {
+        console.log(mediaItem);
+    }
 }
